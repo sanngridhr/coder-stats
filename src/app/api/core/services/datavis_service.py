49fee36e22fba4edeeb0c6ab8d1d5dcc4dd80d6d
Sanngriðr <sanngridhr@yamadachi.quest>
2026-02-14 18:03:15 +0200

    feat[datavis]: add piechart colours

--- src/app/api/core/services/datavis_service.py
+++ src/app/api/core/services/datavis_service.py
@@ -7,6 +7,22 @@ from fastapi import Depends, Response
 from matplotlib.figure import Figure
 from matplotlib.patches import Circle
 
+PIE_RADIUS = 1.0
+PIE_GAP = 0.05
+PIE_WIDTH = 0.2
+
+PIE_COLOURS = [
+    "#AF3029",  # red
+    "#BC5215",  # orange
+    "#AD8301",  # yellow
+    "#66800B",  # green
+    "#24837B",  # cyan
+    "#205EA6",  # blue
+    "#5E409D",  # purple
+    "#A02F6F",  # magenta
+]
+PIE_COLOURS_LEN = len(PIE_COLOURS)
+
 
 class DataVisService:
     @cached(ttl=300, noself=True)
@@ -21,10 +37,6 @@ class DataVisService:
 
     @cached(ttl=300, noself=True)
     async def get_donut_chart(self, data: dict[str, int | float]) -> Figure:
-        radius: float = 1.0
-        gap: float = 0.05
-        width: float = 0.2
-
         fig, ax = plt.subplots()
 
         lumped_data: dict[str, int | float] = await self.__lump_small_data(data)
@@ -34,18 +46,25 @@ class DataVisService:
         ax.pie(
             x=values,
             labels=keys,
-            radius=radius,
-            wedgeprops={"width": width, "edgecolor": "#FFFCF0"},
-            explode=[gap] * len(values),
+            radius=PIE_RADIUS,
+            colors=PIE_COLOURS,
+            counterclock=False,
+            startangle=90,
+            wedgeprops={"width": PIE_WIDTH, "edgecolor": "#FFFCF0"},
+            explode=[PIE_GAP] * len(values),
         )
 
-        ax.add_artist(Circle(xy=(0, 0), radius=radius - width, fc="#FFFCF0"))
+        ax.add_artist(Circle(xy=(0, 0), radius=PIE_RADIUS - PIE_WIDTH, fc="#FFFCF0"))
 
         return fig
 
-    async def __lump_small_data(self, data: dict[str, int | float], threshold: float = 3000) -> dict[str, int | float]:
-        major: dict[str, int | float] = {k: v for k, v in data.items() if v >= threshold}
-        minor_sum: int | float = sum(v for v in data.values() if v < threshold)
+    async def __lump_small_data(
+        self,
+        data: dict[str, int | float],
+        major_num=(PIE_COLOURS_LEN - 1),
+    ) -> dict[str, int | float]:
+        major: dict[str, int | float] = {k: v for k, v in list(data.items())[:major_num]}
+        minor_sum: int | float = sum(v for v in list(data.values())[major_num + 1 :])
 
         if minor_sum > 0:
             major["Other"] = minor_sum
